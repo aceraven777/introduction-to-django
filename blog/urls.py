f5a3d6f2a7d6a1383f1e4e235b45eb94b1a50027
Aristheo Lacdao <aceraven777@gmail.com>
2018-06-26 20:00:20 +0800

    Blog views and migrations

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.views.generic import ListView, DetailView
+from blog.models import Post
+
+urlpatterns = [
+    path(
+        '',
+        ListView.as_view(
+            queryset=Post.objects.all().order_by('-date')[:25],
+            template_name='blog/blog.html'
+        ),
+    ),
+]
